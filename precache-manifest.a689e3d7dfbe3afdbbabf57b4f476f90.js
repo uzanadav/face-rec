a8402f1c741dd22803da7e1f3934166c6288d279
uzanadav <uzanadav@gmail.com>
2020-04-25 16:13:22 +0300

    Updates

---
+++ precache-manifest.a689e3d7dfbe3afdbbabf57b4f476f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167528f21013a164fe12fb383d79de2c",
+    "url": "/face-rec/index.html"
+  },
+  {
+    "revision": "f714d8560d62120b3060",
+    "url": "/face-rec/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "27ac37902ccdc1298c1c",
+    "url": "/face-rec/static/css/main.57437240.chunk.css"
+  },
+  {
+    "revision": "f714d8560d62120b3060",
+    "url": "/face-rec/static/js/2.3a35c5f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-rec/static/js/2.3a35c5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ac37902ccdc1298c1c",
+    "url": "/face-rec/static/js/main.4dbce31f.chunk.js"
+  },
+  {
+    "revision": "0b5684375128fa3482fd",
+    "url": "/face-rec/static/js/runtime-main.3bc99a6d.js"
+  }
+]);
